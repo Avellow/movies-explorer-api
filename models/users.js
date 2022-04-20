9c4b7182a1430ee478a0b751f40657599584d702
Avellow <avellow@yandex.ru>
2022-04-20 16:59:01 +0300

    feature добавить метод проверки данных пользователя при логине

--- models/users.js
+++ models/users.js
@@ -26,4 +26,25 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this
+    .findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль!')); // заменить на кастомную
+      }
+
+      return bcrypt
+        .compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль!')); // заменить на кастомную
+          }
+
+          return user;
+        });
+    });
+};
+
 module.exports = mongoose.model('user', userSchema);
