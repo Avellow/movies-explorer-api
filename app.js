c5b5341b5535f7a8863eebcc8990289e94903baa
Avellow <avellow@yandex.ru>
2022-04-21 12:54:34 +0300

    feature использовать регистрацию

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { login } = require("./controllers/user");
+const { login, createUser} = require("./controllers/user");
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 // конец хард-коддинга
 
 app.post('/signin', login);
-
+app.post('/signup', createUser);
 
 app.use('/movies', require('./routes/movies'));
 
