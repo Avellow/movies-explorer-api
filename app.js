e750eeb3aa3edc5c3f7937287853e433c23b1c66
Avellow <avellow@yandex.ru>
2022-04-21 12:43:49 +0300

    feature использовать роут для фильмов

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// временный хард-код. Удалить при реализации авторизации
+app.use((req, res, next) => {
+  req.user = {
+    _id: '625fe0fd077c3e9c8339dd36',
+  };
+  next();
+});
+// конец хард-коддинга
+
+
+
+app.use('/movies', require('./routes/movies'));
+
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(PORT, () => {
