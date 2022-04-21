7c46015343aff2c4779611c95ab237e3e62a51cf
Avellow <avellow@yandex.ru>
2022-04-21 18:21:53 +0300

    feature промежуточная валидация данных фильма

---
+++ middlewares/movieValidator.js
@@ -0,0 +1,30 @@
+const { celebrate, Joi } = require('celebrate');
+const isUrl = require('validator/lib/isURL');
+
+const checkUrl = (url) => {
+  if (!isUrl(url, { require_protocol: true })) {
+    throw new Error('Невалидная ссылка');
+  }
+
+  return url;
+};
+
+const movieValidator = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom(checkUrl, 'image link validation'),
+    trailer: Joi.string().required().custom(checkUrl, 'trailer link validation'),
+    thumbnail: Joi.string().required().custom(checkUrl, 'thumbnail link validation'),
+    movieId: Joi.string().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+module.exports = {
+  movieValidator,
+};
--- routes/movies.js
+++ routes/movies.js
@@ -4,9 +4,10 @@ const {
   createMovie,
   deleteMovie,
 } = require('../controllers/movie');
+const { movieValidator } = require('../middlewares/movieValidator');
 
 router.get('/', getMovies);
-router.post('/', createMovie);
+router.post('/', movieValidator, createMovie);
 router.delete('/:movieId', deleteMovie);
 
 module.exports = router;
