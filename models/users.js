f0170d916bdc29a09af95f56136e2200f6334fda
Avellow <avellow@yandex.ru>
2022-04-24 13:56:17 +0300

    modify использовать кастомную ошибку при неудачном логине

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
-// const AuthError = require('../errors/AuthError');
+const AuthError = require('../errors/AuthError');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -32,14 +32,14 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
     .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль!')); // заменить на кастомную
+        return Promise.reject(new AuthError('Неправильные почта или пароль!'));
       }
 
       return bcrypt
         .compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль!')); // заменить на кастомную
+            return Promise.reject(new AuthError('Неправильные почта или пароль!'));
           }
 
           return user;
